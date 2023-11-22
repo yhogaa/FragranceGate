f73b0a7f5ff335a172f6bc90acafa8fa8396a6e9
Fadrian Yhoga Pratama <fadrian.yhoga@ui.ac.id>
2023-11-22 12:08:03 +0700

    register fix

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,176 +1,117 @@
-import 'dart:convert' as convert;
-import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:inventory_flutter/screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:inventory_flutter/screens/login.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
 class RegisterPage extends StatefulWidget {
-  const RegisterPage({Key? key}) : super(key: key);
-  static const ROUTE_NAME = '/register';
+  const RegisterPage({super.key});
 
   @override
-  State<RegisterPage> createState() => _RegisterPageState();
+  _RegisterPageState createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  final _formKey = GlobalKey<FormState>();
-
-  String username = "";
-  String password = "";
-  String confirmPassword = "";
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
     return Scaffold(
-          appBar: AppBar(
-              title: const Text('Register'),
-          ),
-          body: Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-
-                      TextFormField(
-                          decoration: InputDecoration(
-                                labelText: "Username",
-                                icon: const Icon(Icons.people),
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(5.0)),
-                              ),
-                              onChanged: (String? value) {
-                                setState(() {
-                                  username = value!;
-                                });
-                              },
-                              onSaved: (String? value) {
-                                setState(() {
-                                  username = value!;
-                                });
-                              },
-                              autovalidateMode:
-                                  AutovalidateMode.onUserInteraction,
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return 'Username tidak boleh kosong';
-                                }
-                                return null;
-                              },
-                            ),
-
-                      const SizedBox(height: 12.0),
-
-                      TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: "Password",
-                            icon: const Icon(
-                              Icons.lock_outline,
-                            ),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0)),
-                          ),
-                          onChanged: (String? value) {
-                            setState(() {
-                              password = value!;
-                            });
-                          },
-                          onSaved: (String? value) {
-                            setState(() {
-                              password = value!;
-                            });
-                          },
-                          autovalidateMode:
-                              AutovalidateMode.onUserInteraction,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Password tidak boleh kosong';
-                            }
-                            return null;
-                          },
-                        ),
-
-                      const SizedBox(height: 24.0),
-
-                      TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                          labelText: "Konfirmasi Password",
-                            icon: const Icon(Icons.lock_outline),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5.0)),
-                          ),
-                          onChanged: (String? value) {
-                            setState(() {
-                              confirmPassword = value!;
-                            });
-                          },
-                          onSaved: (String? value) {
-                            setState(() {
-                              confirmPassword = value!;
-                            });
-                          },
-                          autovalidateMode:
-                              AutovalidateMode.onUserInteraction,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Password tidak boleh kosong';
-                            }
-                            return null;
-                          },
-                        ),
-
-                      const SizedBox(height: 36.0),
-
-                      ElevatedButton(
-                          onPressed: () async {
-                             if (_formKey.currentState!.validate()) {
-                                  // Submit to Django server and wait for response
-                                  final response = await request.postJson(
-                                      "https://fadrian-yhoga-tugas.pbp.cs.ui.ac.id/auth/register/",
-                                      convert.jsonEncode(<String, String>{
-                                        'username': username,
-                                        'password': password,
-                                        'confirmPassword': confirmPassword,
-                                      }));
-                                  if (response['status'] == 'Berhasil') {
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(const SnackBar(
-                                      content: Text(
-                                          "Akun berhasil terdaftar!"),
-                                    ));
-                                    Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              const LoginPage()),
-                                    );
-                                  } else {
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(const SnackBar(
-                                      content: Text(
-                                          "Akun gagal terdaftar! Silahkan coba lagi."),
-                                    ));
-                                  }
-                                }
-
-                          },
-                          child: const Text('Register'),
-                      ),
-                      const SizedBox(height: 24.0),
-                        ElevatedButton(
-                            onPressed: () async {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                                );
-                            },
-                            child: const Text('Login'),
-                        )
-                  ],
+      appBar: AppBar(
+        title: const Text(
+          'Register',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                labelStyle: TextStyle(color: Colors.black),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
               ),
-          ),
-      );
-    }
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(color: Colors.black),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 12.0),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () async {
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+
+                final response = await request.post(
+                    // "http://127.0.0.1:8000/auth/register/",
+                    "https://fadrian-yhoga-tugas.pbp.cs.ui.ac.id/auth/register/",
+                    {
+                      'username': username,
+                      'password': password,
+                    });
+
+                if (response['status']) {
+                  String message = response['message'];
+                  
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                  ScaffoldMessenger.of(context)
+                    ..hideCurrentSnackBar()
+                    ..showSnackBar(SnackBar(content: Text("$message")));
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text('Register failed.'),
+                      content: Text(response['message']),
+                      actions: [
+                        TextButton(
+                          child: const Text('OK'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              child: const Text('Register'),
+            ),
+            const SizedBox(height: 12.0),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to Login
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: const Text('Login'),
+            )
+          ],
+        ),
+      ),
+    );
   }
+}
